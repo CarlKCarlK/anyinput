d338052ad52624fe5eccc3fddef83c1af9e533fb
Carl Kadie <carlk@msn.com>
2022-09-22 14:11:02 -0700

    Run function through null fold_item_fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,52 @@
 // cmk rename to input-like-derive (or derive-input-like)
+
+// Parsing based on https://github.com/dtolnay/syn/tree/master/examples/trace-var
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{
+    fold::{self, Fold},
+    parse_macro_input, Expr, ItemFn, Stmt,
+};
+
+struct Args {}
+
+impl Fold for Args {
+    fn fold_expr(&mut self, e: Expr) -> Expr {
+        match e {
+            // Expr::Assign(e) => {
+            //     // if self.should_print_expr(&e.left) {
+            //     //     self.assign_and_print(*e.left, &e.eq_token, *e.right)
+            //     // } else {
+            //     //     Expr::Assign(fold::fold_expr_assign(self, e))
+            //     // }
+            //     panic!("Assign not supported");
+            // }
+            // Expr::AssignOp(e) => {
+            //     // if self.should_print_expr(&e.left) {
+            //     //     self.assign_and_print(*e.left, &e.op, *e.right)
+            //     // } else {
+            //     //     Expr::AssignOp(fold::fold_expr_assign_op(self, e))
+            //     // }
+            //     panic!("AssignOp not supported");
+            // }
+            _ => fold::fold_expr(self, e),
+        }
+    }
+
+    fn fold_stmt(&mut self, s: Stmt) -> Stmt {
+        match s {
+            // Stmt::Local(s) => {
+            //     // if s.init.is_some() && self.should_print_pat(&s.pat) {
+            //     //     self.let_and_print(s)
+            //     // } else {
+            //     //     Stmt::Local(fold::fold_local(self, s))
+            //     // }
+            //     panic!("Local not supported");
+            // }
+            _ => fold::fold_stmt(self, s),
+        }
+    }
+}
 
 #[proc_macro_attribute]
 pub fn input_like(_: TokenStream, input: TokenStream) -> TokenStream {
@@ -12,7 +57,10 @@ pub fn input_like(_: TokenStream, input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as ItemFn);
     // panic!("item: {:#?}", &input);
 
-    TokenStream::from(quote!(#input))
+    let mut args = Args {};
+    let output = args.fold_item_fn(input);
+
+    TokenStream::from(quote!(#output))
 
     // let ast: DeriveInput = syn::parse(input.clone()).unwrap();
     // // panic!("ast: {:#?}", &ast);
