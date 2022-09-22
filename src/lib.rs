5d641af7038a8fd4b04815c26dbe0f947f7fca9e
Carl Kadie <carlk@msn.com>
2022-09-22 09:43:07 -0700

    test any_path_part_count w/o macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn input_like(_: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+        // cmk update tests
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
