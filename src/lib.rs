b41ceb5a2ad65a4a063defa820f0b2cb814c65ba
Carl Kadie <carlk@msn.com>
2022-09-22 13:41:19 -0700

    parsing function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
 // cmk rename to input-like-derive (or derive-input-like)
 use proc_macro::TokenStream;
-use quote::{quote, ToTokens};
-use syn::{self, parse_macro_input};
+use syn::{self, parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
-pub fn input_like(_: TokenStream, item: TokenStream) -> TokenStream {
-    item
+pub fn input_like(_: TokenStream, input: TokenStream) -> TokenStream {
+    // cmk 0 item
+    // panic!("item: {:#?}", &item);
+    // item
+
+    let input = parse_macro_input!(input as ItemFn);
+    panic!("item: {:#?}", &input);
+
+    // let ast: DeriveInput = syn::parse(input.clone()).unwrap();
+    // // panic!("ast: {:#?}", &ast);
+    // input
+    // let ast: syn::DeriveInput = parse_macro_input!(item);
+    // panic!("ast: {:#?}", ast);
+
     // // Construct a representation of Rust code as a syntax tree
     // // that we can manipulate
-    // let ast = parse_macro_input!(item);
+    // let ast: syn::DeriveInput = parse_macro_input!(item);
+    // TokenStream::from(ast).into()
 
     // // panic!("My function name is: <{}>", ast.ident.to_string());
     // // TokenStream::new() //from(impl_input_like(&ast))
@@ -17,41 +29,41 @@ pub fn input_like(_: TokenStream, item: TokenStream) -> TokenStream {
     // impl_input_like(&ast)
 }
 
-fn impl_input_like(ast: &syn::DeriveInput) -> TokenStream {
-    ast.into_token_stream().into()
-    // let data = &ast.data;
-    // match data
-    // {
-    //     //syn::DataFn { attrs, vis, sig, block }
-    //     Fn() { attrs, vis, sig, block } => {
-    //         let name = &ast.ident;
-    //         let gen = quote! {
-    //             impl InputLike for #name {
-    //                 fn input_like(&self) -> Result<(), anyhow::Error> {
-    //                     // ...
-    //                 }
-    //             }
-    //         };
-    //         gen.into()
-    //     },
-    //     syn::Data::Fn() => {
-    //         panic!("cmk"),
+// fn impl_input_like(ast: &syn::DeriveInput) -> TokenStream {
+//     ast.into_token_stream().into()
+//     // let data = &ast.data;
+//     // match data
+//     // {
+//     //     //syn::DataFn { attrs, vis, sig, block }
+//     //     Fn() { attrs, vis, sig, block } => {
+//     //         let name = &ast.ident;
+//     //         let gen = quote! {
+//     //             impl InputLike for #name {
+//     //                 fn input_like(&self) -> Result<(), anyhow::Error> {
+//     //                     // ...
+//     //                 }
+//     //             }
+//     //         };
+//     //         gen.into()
+//     //     },
+//     //     syn::Data::Fn() => {
+//     //         panic!("cmk"),
 
-    //     },
-    //             _ => panic!("Only named fields are supported"),
-    //}
-    // let func = data
-    // TokenStream::from(&ast)
-    // let name = &ast.ident;
-    // let gen = quote! {
-    //     impl HelloMacro for #name {
-    //         fn hello_macro() {
-    //             println!("Hello, Macro! My name is {}", stringify!(#name));
-    //         }
-    //     }
-    // };
-    // gen.into()
-}
+//     //     },
+//     //             _ => panic!("Only named fields are supported"),
+//     //}
+//     // let func = data
+//     // TokenStream::from(&ast)
+//     // let name = &ast.ident;
+//     // let gen = quote! {
+//     //     impl HelloMacro for #name {
+//     //         fn hello_macro() {
+//     //             println!("Hello, Macro! My name is {}", stringify!(#name));
+//     //         }
+//     //     }
+//     // };
+//     // gen.into()
+// }
 
 #[cfg(test)]
 mod tests {
