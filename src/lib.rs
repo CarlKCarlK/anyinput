7725a150bf5980bdedd55fe0d0bfefaa9e28401f
Carl Kadie <carlk@msn.com>
2022-09-24 16:10:40 -0700

    still passes test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 // cmk rename to input-like-derive (or derive-input-like)
+// cmk remove cargo stuff features of syn no longer needed.
+// cmk create unique names for macros with gensym
+// cmk look a syn's macro workshop examples
 
 // Parsing based on https://github.com/dtolnay/syn/tree/master/examples/trace-var
 use proc_macro::TokenStream;
@@ -61,60 +64,7 @@ pub fn input_like(_: TokenStream, input: TokenStream) -> TokenStream {
     let output = args.fold_item_fn(input);
 
     TokenStream::from(quote!(#output))
-
-    // let ast: DeriveInput = syn::parse(input.clone()).unwrap();
-    // // panic!("ast: {:#?}", &ast);
-    // input
-    // let ast: syn::DeriveInput = parse_macro_input!(item);
-    // panic!("ast: {:#?}", ast);
-
-    // // Construct a representation of Rust code as a syntax tree
-    // // that we can manipulate
-    // let ast: syn::DeriveInput = parse_macro_input!(item);
-    // TokenStream::from(ast).into()
-
-    // // panic!("My function name is: <{}>", ast.ident.to_string());
-    // // TokenStream::new() //from(impl_input_like(&ast))
-
-    // // Build the trait implementation
-    // impl_input_like(&ast)
 }
-
-// fn impl_input_like(ast: &syn::DeriveInput) -> TokenStream {
-//     ast.into_token_stream().into()
-//     // let data = &ast.data;
-//     // match data
-//     // {
-//     //     //syn::DataFn { attrs, vis, sig, block }
-//     //     Fn() { attrs, vis, sig, block } => {
-//     //         let name = &ast.ident;
-//     //         let gen = quote! {
-//     //             impl InputLike for #name {
-//     //                 fn input_like(&self) -> Result<(), anyhow::Error> {
-//     //                     // ...
-//     //                 }
-//     //             }
-//     //         };
-//     //         gen.into()
-//     //     },
-//     //     syn::Data::Fn() => {
-//     //         panic!("cmk"),
-
-//     //     },
-//     //             _ => panic!("Only named fields are supported"),
-//     //}
-//     // let func = data
-//     // TokenStream::from(&ast)
-//     // let name = &ast.ident;
-//     // let gen = quote! {
-//     //     impl HelloMacro for #name {
-//     //         fn hello_macro() {
-//     //             println!("Hello, Macro! My name is {}", stringify!(#name));
-//     //         }
-//     //     }
-//     // };
-//     // gen.into()
-// }
 
 #[cfg(test)]
 mod tests {
